93f1ab196e3894f84737bdda373cce223bca5173
robergasp <silent.jaguar@gmail.com>
2021-10-24 16:23:04 +0300

    Lesson_1_last

--- app/src/main/java/com/example/traveltospace/POD/PODState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.traveltospace.POD
-
-sealed class PODState{
-    data class Success(val serverResponseData: PODDataObj): PODState()
-    //data class Success(val podData:List<PODDataObj>): PODState()
-    data class Error (val error:Throwable): PODState()
-}
--- app/src/main/java/com/example/traveltospace/repository/PODRepositoryInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.example.traveltospace.repository
-
-import com.example.traveltospace.POD.PODDataObj
-
-interface PODRepositoryInterface {
-    fun getPODFromInternet(date:String):List<PODDataObj>
-    //fun getPODFromInternet(apiKey:String, date:String):PODDataObj
-}
